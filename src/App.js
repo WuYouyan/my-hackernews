8c7d886ad81db8b6162c92d1eff9054e0feeb40e
Youyan WU <wuyouyan1@gmail.com>
2020-05-17 15:04:24 +0200

    React foundation - Controlled component

--- src/App.js
+++ src/App.js
@@ -22,28 +22,66 @@ const list = [
   },
 ];
 
+const isSearched = (searchTerm) => (item) => 
+item.title.toLowerCase().includes(searchTerm.toLowerCase()); 
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      list,
+      searchTerm: "",
+    };
+
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
   render() {
     let firstName = "Victor";
     let lastNmae = "Zhao";
+    const { searchTerm, list } = this.state;
     return (
       <div className="App App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {list.map(item =>
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>{" "} 
-            </span>
-            <span>{item.author} </span>
-            <span>{item.num_comments} </span>
-            <span>{item.points}</span>
-          </div>  
-        )}
+        <form>
+          <input type="text" 
+            placeholder="search"
+            value={searchTerm}
+            onChange={this.onSearchChange}/>
+        </form>
+        <div>
+          {list.filter(isSearched(searchTerm)).map(item =>
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>{" "} 
+              </span>
+              <span>{item.author} </span>
+              <span>{item.num_comments} </span>
+              <span>{item.points}</span>
+              <span>
+                <button onClick={()=>this.onDismiss(item.objectID)} type="button">dismiss</button>
+              </span>
+            </div>  
+          )}
+        </div>
         <p>Hello {firstName} {lastNmae}, welcome to Hacknews</p>
       </div>
     );
   }
 }
+
 
 export default App;
