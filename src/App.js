5c9db8cc9ef52610dd8733bded06c5c9710fe713
Youyan WU <wuyouyan1@gmail.com>
2020-05-17 17:51:26 +0200

    React foundation - Reusable component

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ class App extends Component {
     return (
       <div className="App App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Search value={searchTerm} onChange={this.onSearchChange}/>
+        <Search value={searchTerm} onChange={this.onSearchChange}>Search</Search>
         <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
         
         <p>Hello {firstName} {lastNmae}, welcome to Hacknews</p>
@@ -65,39 +65,39 @@ class App extends Component {
   }
 }
 
-class Search extends Component {
-  render() {
-    const { value, onChange } = this.props;
-    return (
-      <form>
-        <input type="text" value={value} onChange={onChange} />
-      </form>
-    );
-  }
-}
+const Search = ({ value, onChange, children }) => 
+    <form>
+      {children}
+      <input
+        type="text"
+        value={value}
+        onChange={onChange}/>
+    </form>
+ 
 
-class Table extends Component {
-  render() {
-    const {list, pattern, onDismiss} = this.props;
-    return (
-      <div>
-          {list.filter(isSearched(pattern)).map(item =>
-            <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>{" "} 
-              </span>
-              <span>{item.author} </span>
-              <span>{item.num_comments} </span>
-              <span>{item.points}</span>
-              <span>
-                <button onClick={()=>onDismiss(item.objectID)} type="button">dismiss</button>
-              </span>
-            </div>  
-          )}
-        </div>
-    )
-  }
-}
+const Table = ({list, pattern, onDismiss}) => 
+  <div>
+    {list.filter(isSearched(pattern)).map(item =>
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>{" "} 
+        </span>
+        <span>{item.author} </span>
+        <span>{item.num_comments} </span>
+        <span>{item.points}</span>
+        <span>
+          <Button onClick={()=>onDismiss(item.objectID)}>dismiss</Button>
+        </span>
+      </div>  
+    )}
+  </div>
 
+const Button = ({ onClick, className='', children }) =>
+  <button 
+    onClick={onClick} 
+    className={className}
+    type="button">
+    {children}
+  </button>
 
 export default App;
