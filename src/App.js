33a1757a256450792df761fd051bd9ac9418f71d
Youyan WU <wuyouyan1@gmail.com>
2020-05-18 17:15:50 +0200

    Use Real API - Pagination fetch data

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '100';
 
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
 
 class App extends Component {
@@ -26,12 +29,20 @@ class App extends Component {
   }
 
   setSearchTopStories(result) {
-    this.setState({ result });
+    const { hits, page } = result;
+
+    const oldHits = page !== 0 ? this.state.result.hits : [];
+    const updatedHits = [ ...oldHits, ...hits ];
+
+    this.setState({ 
+      result: { hits: updatedHits, page } 
+    });
   }
 
-  fetchSearchTopStories(searchTerm) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
-      .then(response => response.json())
+  fetchSearchTopStories(searchTerm, page = 0) {
+    const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${
+      page}&${PARAM_HPP}${DEFAULT_HPP}`;
+    fetch(url).then(response => response.json())
       .then(result => this.setSearchTopStories(result))
       .catch(e => e);
   }
@@ -63,6 +74,7 @@ class App extends Component {
   render() {
 
     const { searchTerm, result } = this.state;
+    const page = (result && result.page) || 0;
     if (!result) {
       return null;
     }
@@ -91,6 +103,9 @@ class App extends Component {
             pattern={searchTerm} 
             onDismiss={this.onDismiss}/>
         }
+        <div className="interactions">
+          <Button onClick={() => this.fetchSearchTopStories(searchTerm,  page + 1)}>More</Button>
+        </div>
       </div>
     );
   }
