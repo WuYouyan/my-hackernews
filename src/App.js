0757f0698f65c87b22d0a790e356eeafd8b7dafb
Youyan WU <wuyouyan1@gmail.com>
2020-05-25 15:26:41 +0200

    Advanced React Component - Referencing DOM elements

--- src/App.js
+++ src/App.js
@@ -135,15 +135,51 @@ class App extends Component {
   }
 }
 
-const Search = ({ value, onChange, onSubmit, children }) => 
-  <form onSubmit={onSubmit}>
-    {children}
-    <input
-      type="text"
-      value={value}
-      onChange={onChange}/>
-      <button type="submit">{children}</button>
-  </form>
+// class Search extends Component {
+//   componentDidMount() {
+//     if (this.input) {
+//       this.input.focus();
+//     }
+//   }
+
+//   render() {
+//     const {
+//       value,
+//       onChange,
+//       onSubmit,
+//       children
+//     } = this.props;
+//     return (
+//       <form onSubmit={onSubmit}>
+//         {children}
+//         <input
+//           type="text"
+//           value={value}
+//           onChange={onChange}
+//           ref = {(node)=>{this.input = node;}}
+//           />
+//         <button type="submit">{children}</button>
+//       </form>
+//     );
+//   }
+// }
+
+
+const Search = ({ value, onChange, onSubmit, children }) => {
+  let input;
+  return (
+    <form onSubmit={onSubmit}>
+      {children}
+      <input
+        type="text"
+        value={value}
+        onChange={onChange}
+        ref={(node) => input = node}
+      />
+        <button type="submit">{children}</button>
+    </form>
+  );
+}
 Search.propTypes = {
   value: PropTypes.string,
   onChange: PropTypes.func,
