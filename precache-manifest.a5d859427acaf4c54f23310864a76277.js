a71fe1775e358a04f7c23c56aea51b633fc95140
Youyan WU <wuyouyan1@gmail.com>
2020-06-02 16:24:46 +0200

    Updates

---
+++ precache-manifest.a5d859427acaf4c54f23310864a76277.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71a6e44a40b9e8f86199b602082c6af8",
+    "url": "/my-hackernews/index.html"
+  },
+  {
+    "revision": "68394cd4ec79717e3312",
+    "url": "/my-hackernews/static/css/2.a5f7c577.chunk.css"
+  },
+  {
+    "revision": "257c75565c32f8b6de20",
+    "url": "/my-hackernews/static/css/main.08197e06.chunk.css"
+  },
+  {
+    "revision": "68394cd4ec79717e3312",
+    "url": "/my-hackernews/static/js/2.8744bb34.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/my-hackernews/static/js/2.8744bb34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "257c75565c32f8b6de20",
+    "url": "/my-hackernews/static/js/main.6dfb0f24.chunk.js"
+  },
+  {
+    "revision": "d3e1fa13d88be943641d",
+    "url": "/my-hackernews/static/js/runtime-main.7c198005.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/my-hackernews/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/my-hackernews/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/my-hackernews/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/my-hackernews/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/my-hackernews/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
